a41deb346ce5f47bb69822c2d1d8a71d8682d242
Alex Mendes da Costa <alexmdac@gmail.com>
2021-04-15 00:16:22 -0700

    remove <iostream> and using namespace std

--- examples/hello/main.cc
+++ examples/hello/main.cc
@@ -1,10 +1,6 @@
-#include <iostream>
-
 #include "examples/hello/hello.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
-
-using namespace std;
 
 DEFINE_string(whom, "c++", "whom to greet");
 DEFINE_bool(json, false, "whether to return json");
@@ -12,7 +8,7 @@ DEFINE_bool(json, false, "whether to return json");
 int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
-  string greeting =
+  std::string greeting =
       FLAGS_json ? GetGreetingJson(FLAGS_whom) : GetGreeting(FLAGS_whom);
   LOG(INFO) << greeting;
 }
